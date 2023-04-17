29d011541a37c2d55846b794d169649e36f44820
Mizuki <mizukikeda@gmail.com>
2023-04-17 12:22:52 +0200

    Feat: Logo addition

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,11 +1,11 @@
 import './style.scss';
-// import logo from '../../assets/logo.png';
+import logo from '../../assets/chef-hat.png';
 // import LoginForm from '../LoginForm';
 
 function AppHeader() {
   return (
     <header className="header">
-      {/* <img src={logo} className="header-logo" alt="Logo oRecipes" /> */}
+      <img src={logo} className="header-logo" alt="Logo oRecipes" />
       {/* <LoginForm /> */}
       <h1>Hello</h1>
     </header>
