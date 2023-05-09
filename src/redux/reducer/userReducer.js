3d61ae8dbb6bef905b72578d50ad5f3754b3c299
Mizuki <mizukikeda@gmail.com>
2023-05-09 16:59:18 +0200

    Feat: handleSubmit auth form

--- src/redux/reducer/userReducer.js
+++ src/redux/reducer/userReducer.js
@@ -1,21 +1,47 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
     logged: false,
     email: '',
     password: '',
     pseudo: '',
+    user: null,
     loading: false,
+    error: null,
 }
 
 const userSlice = createSlice({
   name: 'users',
   initialState,
-  reducers: {},
-  extraReducers: {},
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+      state.loading = false;
+      state.error = null;
+    }, 
+    setLoading: state => {
+      state.loading = true;
+    },
+    setError: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    }
+  },
+  // extraReducers: {},
 })
 
 // Action creators are generated for each case reducer function
-// export const { getrecipes } = userSlice.actions;
+export const { setUser, setLoading, setError } = userSlice.actions;
+
+export const login = (email, password) => async dispatch => {
+  dispatch(setLoading());
+  try {
+    const response = await axios.post('http://localhost:4001', { email, password });
+    dispatch(setUser(response.data));
+  } catch (error) {
+    dispatch(setError(error.message));
+  }
+}
 
 export default userSlice.reducer;
